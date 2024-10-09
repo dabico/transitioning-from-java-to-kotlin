f06e1eb89f762ad2a0d1806c4643c5dfa81f5683
Ozren Dabić <ozren.dabic@jetbrains.com>
2024-10-09 17:37:51 +0200

    Add Ktor skeleton code

---
+++ ktor-store-demo/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "1.7.10"
+    id("io.ktor.plugin") version "2.3.12"
+}
+
+group = "io.github.dabico.store"
+version = "0.0.1"
+
+application {
+    mainClass.set("io.github.dabico.store.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:1.5.9")
+}
---
+++ ktor-store-demo/src/main/kotlin/io/github/dabico/store/plugins/Routing.kt
@@ -0,0 +1,9 @@
+package io.github.dabico.store.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+    }
+}
