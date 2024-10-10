d8b721fe215ed385632c5a420db9fb1f938a96ca
Ozren Dabić <ozren.dabic@jetbrains.com>
2024-10-10 09:48:11 +0200

    Add root route

--- ktor-store-demo/src/main/kotlin/io/github/dabico/store/plugins/Routing.kt
+++ ktor-store-demo/src/main/kotlin/io/github/dabico/store/plugins/Routing.kt
@@ -1,11 +1,16 @@
 package io.github.dabico.store.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
+        get {
+            call.response.status(HttpStatusCode.OK)
+        }
+
         get("/products") {
             call.respondText { "List of products!" }
         }
