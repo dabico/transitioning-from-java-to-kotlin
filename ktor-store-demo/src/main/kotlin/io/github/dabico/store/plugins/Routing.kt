2ef0ef23eb489188d0d33e7ecc4ad9e12ed52690
Ozren Dabić <ozren.dabic@jetbrains.com>
2024-10-10 09:40:46 +0200

    Define first REST endpoint

--- ktor-store-demo/src/main/kotlin/io/github/dabico/store/plugins/Routing.kt
+++ ktor-store-demo/src/main/kotlin/io/github/dabico/store/plugins/Routing.kt
@@ -1,9 +1,13 @@
 package io.github.dabico.store.plugins
 
 import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
+        get("/products") {
+            call.respondText { "List of products!" }
+        }
     }
 }
